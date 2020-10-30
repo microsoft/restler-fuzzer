2fc2a61423882df8cbe88befaa4e04da06a1c3ce
rifiles <v-rifile@microsoft.com>
2020-10-30 11:14:40 -0700

    Code review update

--- restler/checkers/invalid_dynamic_object_rule.py
+++ restler/checkers/invalid_dynamic_object_rule.py
@@ -128,7 +128,7 @@ class InvalidDynamicObjectChecker(CheckerBase):
         for i in range(1, len(data), 2):
             consumer_types.append(dependencies.get_variable(data[i]))
 
-        default_invalids = [f'{VALID_REPLACE_STR}?injected_query_string',\
+        default_invalids = [f'{VALID_REPLACE_STR}?injected_query_string=123',\
                             f'{VALID_REPLACE_STR}/?/',\
                             f'{VALID_REPLACE_STR}??',\
                             f'{VALID_REPLACE_STR}/{VALID_REPLACE_STR}',\
