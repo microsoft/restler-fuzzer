533e2ec8b251194dacf1fd7be2a4a96d96b85e05
Marina Polishchuk <marinapo@microsoft.com>
2020-11-20 21:58:21 -0800

    Workaround for issue #61.
    NSWag currently fails to parse a header definition with a boolean required property.
    
    Work around this by removing the required.
    There is currently no impact for RESTler correctness, because it does not
    exclude optional properties.

---
+++ src/compiler/Restler.Compiler.Test/SchemaTests.fs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Restler.Test
+open System
+open System.IO
+open Xunit
+open Restler.Test.Utilities
+open Restler.Config
+open Microsoft.FSharpLu.File
+
+/// This test suite contains coverage for corner cases / regression tests in how
+/// RESTler handles the API specification schema.
+[<Trait("TestCategory", "ApiSpecSchema")>]
+module ApiSpecSchema =
+    type SchemaTests(ctx:Fixtures.TestSetupAndCleanup, output:Xunit.Abstractions.ITestOutputHelper) =
+
+        let compileSpec specFileName =
+            let filePath = Path.Combine(Environment.CurrentDirectory, specFileName)
+            let config = { Restler.Config.SampleConfig with
+                             IncludeOptionalParameters = true
+                             GrammarOutputDirectoryPath = Some ctx.testRootDirPath
+                             ResolveBodyDependencies = true
+                             UseBodyExamples = Some false
+                             SwaggerSpecFilePath = Some [filePath]
+                             CustomDictionaryFilePath = None
+                         }
+            Restler.Workflow.generateRestlerGrammar None config
+            let grammarOutputFilePath =
+                config.GrammarOutputDirectoryPath.Value ++ Restler.Workflow.Constants.DefaultRestlerGrammarFileName
+            Assert.True(File.Exists(grammarOutputFilePath))
+
+        [<Fact>]
+        let ``required header is parsed successfully`` () =
+            compileSpec @"swagger\schemaTests\requiredHeader.yml"
+
+
+        interface IClassFixture<Fixtures.TestSetupAndCleanup>
