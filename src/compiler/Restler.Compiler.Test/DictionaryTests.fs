a3c1a093e10490c7ebb8d8bd4ec965d345491173
marina-p <marinapo@microsoft.com>
2020-12-02 21:27:24 -0800

    Fix regression in user-specified uuid4_suffix (#96)
    The user should be able to specify a restler_custom_payload_uuid4_suffix
    in the dictionary, similarly to how restler_custom_payload is specified.
    This use case was regressed when auto-inferred uuid4_suffixes were added.
    
    This change fixes the bug and adds a test for the user-specified case.
    
    Closes #88.

--- src/compiler/Restler.Compiler.Test/DictionaryTests.fs
+++ src/compiler/Restler.Compiler.Test/DictionaryTests.fs
@@ -34,4 +34,29 @@ module Dictionary =
             let message = sprintf "Grammar Does not match baseline.  First difference: %A" grammarDiff
             Assert.True(grammarDiff.IsNone, message)
 
+        [<Fact>]
+        /// Test for custom payload uuid suffix
+        let ``path and body parameter set to the same uuid suffix payload`` () =
+            let grammarOutputDirPath = @"c:\temp\restlertest" //ctx.testRootDirPath
+            let config = { Restler.Config.SampleConfig with
+                             IncludeOptionalParameters = true
+                             GrammarOutputDirectoryPath = Some grammarOutputDirPath
+                             ResolveBodyDependencies = true
+                             ResolveQueryDependencies = true
+                             UseBodyExamples = Some false
+                             UseQueryExamples = Some false
+                             SwaggerSpecFilePath = Some [(Path.Combine(Environment.CurrentDirectory, @"swagger\dictionaryTests\multipleIdenticalUuidSuffix.json"))]
+                             CustomDictionaryFilePath = Some (Path.Combine(Environment.CurrentDirectory, @"swagger\dictionaryTests\multipleIdenticalUuidSuffixDict.json"))
+                         }
+            Restler.Workflow.generateRestlerGrammar None config
+
+            let grammarFilePath = Path.Combine(grammarOutputDirPath,
+                                               Restler.Workflow.Constants.DefaultRestlerGrammarFileName)
+            let grammar = File.ReadAllText(grammarFilePath)
+
+            // The grammar should contain both of the custom payload uuid suffixes from the dictionary
+            Assert.True(grammar.Contains("""primitives.restler_custom_payload_uuid4_suffix("resourceId"),"""))
+            Assert.True(grammar.Contains("""primitives.restler_custom_payload_uuid4_suffix("/resource/id"),"""))
+
+
         interface IClassFixture<Fixtures.TestSetupAndCleanup>
