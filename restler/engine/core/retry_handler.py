6d641061e32d7adb3c2c8f7f1354e945793bf818
ajaip <ajaikpasupuleti@gmail.com>
2022-12-01 15:58:05 -0800

    Update retry handler with copyright and docstrings

--- restler/engine/core/retry_handler.py
+++ restler/engine/core/retry_handler.py
@@ -1,6 +1,12 @@
-import time 
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+
+import time
 import sys
+
 class RetryHandler:
+    """ Utilities for handling retries """
     def __init__(self, type="Linear", max_retries=5, delay=5, max_delay=60):
         self.type = type
         self.max_retries = max_retries
@@ -9,11 +15,20 @@ class RetryHandler:
         self.__num_retries = 0
 
     def can_retry(self):
+        """ Determine if a retry should be executed
+        @return: Whether or not a retry should be executed
+        @rtype : Bool
+        """
+
         if self.__num_retries < self.max_retries:
             return True
         else:
             return False
     def wait_for_next_retry(self):
+        """ Sleep until next retry should be attempted
+        @return: None
+        @rtype : None
+        """
         if not self.can_retry():
             sys.exit(-1)
 
