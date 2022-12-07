36837920ecb66d8d6ee632c2a5e12d5263d26569
ajaip <ajaikpasupuleti@gmail.com>
2022-12-07 13:08:07 -0800

    Split token formatting across multiple lines

--- restler/unit_tests/authentication_test_files/unit_test_server_auth_module.py
+++ restler/unit_tests/authentication_test_files/unit_test_server_auth_module.py
@@ -3,5 +3,9 @@
 
 def acquire_token(log):
     log("Returning a valid token")
-    return ("{'user1':{}, 'user2':{}}\n" + "Authorization: valid_module_unit_test_token\n" + "Authorization: shadow_unit_test_token")
-
+    token_lines = [
+        "{'user1':{}, 'user2':{}}",
+        "Authorization: valid_module_unit_test_token",
+        "Authorization: shadow_unit_test_token"
+    ]
+    return "\n".join(token_lines)
