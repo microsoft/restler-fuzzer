f17285e5dd1b91c020dafbadc3a6c109d62845d2
marina-p <marinapo@microsoft.com>
2025-01-23 16:28:52 -0800

    Update RESTler version to 9.2.5 (#933)
    Updates in this version:
    - bug fixes and documentation updates

--- src/driver/Program.fs
+++ src/driver/Program.fs
@@ -14,7 +14,7 @@ open Microsoft.FSharpLu.Diagnostics.Process
 open Restler.Telemetry
 
 [<Literal>]
-let CurrentVersion = "9.2.4"
+let CurrentVersion = "9.2.5"
 let EngineErrorCode = -2
 
 let exitRestler status =
