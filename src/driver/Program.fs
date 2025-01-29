d8d151f78453ffb144637cea60dde32059f9f188
Marina Polishchuk <marinapo@microsoft.com>
2025-01-29 11:10:53 -0800

    Update RESTler version to 9.3.1
    New in this version:
    - Upgrade python to 3.12.8

--- src/driver/Program.fs
+++ src/driver/Program.fs
@@ -14,7 +14,7 @@ open Microsoft.FSharpLu.Diagnostics.Process
 open Restler.Telemetry
 
 [<Literal>]
-let CurrentVersion = "9.3.0"
+let CurrentVersion = "9.3.1"
 let EngineErrorCode = -2
 
 let exitRestler status =
